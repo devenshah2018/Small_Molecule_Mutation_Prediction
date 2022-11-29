24df8feb80b581b22219659e45cd96b4aa4e8994
devenshah2018 <46404643+devenshah2018@users.noreply.github.com>
2022-11-28 20:06:33 -0800

    Data preprocessing
    Encoded non-numerical columns, removed constant/quasi-constant columns

--- driver.py
+++ driver.py
@@ -1,7 +1,7 @@
 '''
 TODO
-- Encode PUBCHEM_COORDINATE_TYPE better
-- Tune hyperparameters
+- Feature selection
+- Tune hyperparams
 '''
 
 # Imports
@@ -12,6 +12,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score, precision_score, recall_score
 from sklearn.metrics import f1_score
 import warnings
+from sklearn.feature_selection import VarianceThreshold
 
 
 def main():
@@ -19,14 +20,45 @@ def main():
     # Ignore warnings
     warnings.filterwarnings("ignore")
 
-    # Build dataframe and remove unusable data
+    # Build dataframe and remove instances with null attributes
     df = pd.read_csv('chemical_compounds.csv')
-    df.drop(labels='PUBCHEM_COORDINATE_TYPE', axis=1, inplace=True)
     df.dropna(inplace=True)
 
-    # Train test split
+    # Encode non-numerical features
+    non_num_column_index = []
+    index = 0
+
+    for data_type in df.dtypes:
+        if data_type == 'object':
+            non_num_column_index.append(index)
+        index = index + 1
+
+    non_num_columns = []
+
+    for index in non_num_column_index:
+        non_num_columns.append(df.columns[index])
+
+    for column in non_num_columns:
+        df[column] = df[column].astype('category')
+        df[column] = df[column].cat.codes
+
+    del non_num_column_index
+    del index
+    del non_num_columns
+    
+    # Separate features and target
     x_data = df.loc[:, df.columns != 'Class']
     y_data = df.loc[:, df.columns == 'Class']
+
+    # Remove constant and quasi-constant features 
+    variance_threshold = VarianceThreshold(threshold=0.005)
+    variance_threshold.fit(x_data)
+    constant_columns = [column for column in x_data.columns if column not in x_data.columns[variance_threshold.get_support()]]
+    x_data.drop(constant_columns, axis=1, inplace=True)
+    del variance_threshold
+    del constant_columns
+
+    # Train-test split
     X_train, X_test, y_train, y_test = train_test_split(x_data, y_data, test_size=0.2, shuffle=True)
 
     # Build SVM model
@@ -34,22 +66,18 @@ def main():
     clf.fit(X_train, y_train)
 
     # Predict
-    y_preds = clf.predict(X_test)
+    predictions = clf.predict(X_test)
     prediction_df = pd.DataFrame()
-    prediction_df['Prediction'] = y_preds
+    prediction_df['Prediction'] = predictions
     prediction_df['Validation'] = y_test['Class'].to_numpy()
 
     # Precision, Recall, F1-Score, Accuracy
-    precision = precision_score(y_test['Class'].to_numpy(), y_preds)
-    recall = recall_score(y_test['Class'].to_numpy(), y_preds)
-    f1 = f1_score(y_test['Class'].to_numpy(), y_preds)
-    accuracy = accuracy_score(y_test['Class'].to_numpy(), y_preds)
-    
-    # Visualization
-    ## Display prediction dataframe
-    # display(prediction_df)
+    precision = precision_score(y_test['Class'].to_numpy(), predictions)
+    recall = recall_score(y_test['Class'].to_numpy(), predictions)
+    f1 = f1_score(y_test['Class'].to_numpy(), predictions)
+    accuracy = accuracy_score(y_test['Class'].to_numpy(), predictions)
 
-    ## Display precision, recall, f1, and/or accuracy
+    # Performance
     print("Precision: " + str(precision))
     print("Recall: " + str(recall))
     print("Accuracy: " + str(accuracy))
@@ -57,5 +85,4 @@ def main():
 
 
 if __name__ == '__main__':
-    for i in range(5):
-        main()
+    main()
